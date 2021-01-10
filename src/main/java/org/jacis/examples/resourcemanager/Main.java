f3c301b87b21c94f1ced31ea26a306ed1086a8a7
Jan Wiemer <jan.wiemer@online,.de>
2021-01-10 19:56:51 +0100

    some formatting

--- src/main/java/org/jacis/examples/resourcemanager/Main.java
+++ src/main/java/org/jacis/examples/resourcemanager/Main.java
@@ -15,74 +15,66 @@ import io.helidon.webserver.WebServer;
  */
 public final class Main {
 
-    /**
-     * Cannot be instantiated.
-     */
-    private Main() {
-    }
+  private Main() {
+    // singleton
+  }
 
-    /**
-     * Application main entry point.
-     * @param args command line arguments.
-     */
-    public static void main(final String[] args) {
-        startServer();
-    }
+  /**
+   * Application main entry point.
+   * 
+   * @param args command line arguments.
+   */
+  public static void main(final String[] args) {
+    startServer();
+  }
 
-    /**
-     * Start the server.
-     * @return the created {@link WebServer} instance
-     */
-    static WebServer startServer() {
-        // load logging configuration
-        LogConfig.configureRuntime();
+  /**
+   * Start the server.
+   * 
+   * @return the created {@link WebServer} instance
+   */
+  static WebServer startServer() {
+    // load logging configuration
+    LogConfig.configureRuntime();
 
-        // By default this will pick up application.yaml from the classpath
-        Config config = Config.create();
+    // By default this will pick up application.yaml from the classpath
+    Config config = Config.create();
 
-        // Build server with JSONP support
-        WebServer server = WebServer.builder(createRouting(config))
-                .config(config.get("server"))
-                .addMediaSupport(JsonpSupport.create())
-                .build();
+    // Build server with JSONP support
+    WebServer server = WebServer.builder(createRouting(config)).config(config.get("server"))
+        .addMediaSupport(JsonpSupport.create()).build();
 
-        // Try to start the server. If successful, print some info and arrange to
-        // print a message at shutdown. If unsuccessful, print the exception.
-        server.start()
-                .thenAccept(ws -> {
-                    System.out.println(
-                            "WEB server is up! http://localhost:" + ws.port() + "/greet");
-                    ws.whenShutdown().thenRun(()
-                            -> System.out.println("WEB server is DOWN. Good bye!"));
-                })
-                .exceptionally(t -> {
-                    System.err.println("Startup failed: " + t.getMessage());
-                    t.printStackTrace(System.err);
-                    return null;
-                });
+    // Try to start the server. If successful, print some info and arrange to
+    // print a message at shutdown. If unsuccessful, print the exception.
+    server.start().thenAccept(ws -> {
+      System.out.println("WEB server is up! http://localhost:" + ws.port() + "/greet");
+      ws.whenShutdown().thenRun(() -> System.out.println("WEB server is DOWN. Good bye!"));
+    }).exceptionally(t -> {
+      System.err.println("Startup failed: " + t.getMessage());
+      t.printStackTrace(System.err);
+      return null;
+    });
 
-        // Server threads are not daemon. No need to block. Just react.
+    // Server threads are not daemon. No need to block. Just react.
 
-        return server;
-    }
+    return server;
+  }
 
-    /**
-     * Creates new {@link Routing}.
-     *
-     * @return routing configured with JSON support, a health check, and a service
-     * @param config configuration of this server
-     */
-    private static Routing createRouting(Config config) {
-        MetricsSupport metrics = MetricsSupport.create();
-        GreetService greetService = new GreetService(config);
-        HealthSupport health = HealthSupport.builder()
-                .addLiveness(HealthChecks.healthChecks())   // Adds a convenient set of checks
-                .build();
+  /**
+   * Creates new {@link Routing}.
+   *
+   * @return routing configured with JSON support, a health check, and a service
+   * @param config configuration of this server
+   */
+  private static Routing createRouting(Config config) {
+    MetricsSupport metrics = MetricsSupport.create();
+    GreetService greetService = new GreetService(config);
+    HealthSupport health = HealthSupport.builder().addLiveness(HealthChecks.healthChecks()) // Adds a convenient set
+        // of checks
+        .build();
 
-        return Routing.builder()
-                .register(health)                   // Health at "/health"
-                .register(metrics)                  // Metrics at "/metrics"
-                .register("/greet", greetService)
-                .build();
-    }
+    return Routing.builder().register(health) // Health at "/health"
+        .register(metrics) // Metrics at "/metrics"
+        .register("/greet", greetService).build();
+  }
 }
