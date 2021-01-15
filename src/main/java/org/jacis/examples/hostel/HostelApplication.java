7cd356af1ca8408402b873c79f021d1b2dd59c0b
Jan Wiemer <jan.wiemer@online,.de>
2021-01-15 21:02:56 +0100

    worked on demo app

--- src/main/java/org/jacis/examples/hostel/HostelApplication.java
+++ src/main/java/org/jacis/examples/hostel/HostelApplication.java
@@ -3,12 +3,16 @@ package org.jacis.examples.hostel;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
+import org.jacis.container.JacisContainer;
 import org.jacis.examples.hostel.greet.GreetResource;
 import org.jacis.examples.hostel.room.RoomResource;
 import org.jacis.examples.hostel.room.RoomStore;
+import org.jacis.extension.persistence.MicrostreamStorage;
 
 import io.helidon.microprofile.server.Server;
 
@@ -17,8 +21,18 @@ public class HostelApplication extends Application {
 
   private static final java.util.logging.Logger log = java.util.logging.Logger.getLogger(RoomStore.class.getName());
 
+  @Inject
+  JacisContainer container;
+  @Inject
+  MicrostreamStorage microstreamStorage;
+
   private HostelApplication() {
     // do not construct
+  }
+
+  @PostConstruct
+  public void postConstruct() {
+    log.info(" work with JacisContainer " + container.toString() + " and MicorostreamStorage " + microstreamStorage.toString());
   }
 
   public static void main(String... args) {
--- src/main/java/org/jacis/examples/hostel/jacis/JacisProvider.java
+++ src/main/java/org/jacis/examples/hostel/jacis/JacisProvider.java
@@ -4,19 +4,28 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 import javax.transaction.TransactionManager;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jacis.container.JacisContainer;
 import org.jacis.extension.persistence.MicrostreamStorage;
 import org.jacis.plugin.txadapter.jta.AbstractJacisTransactionAdapterJTA;
 
 import one.microstream.storage.configuration.Configuration;
+import one.microstream.storage.restservice.StorageRestService;
+import one.microstream.storage.restservice.StorageRestServiceResolver;
 import one.microstream.storage.types.EmbeddedStorageManager;
 
 @ApplicationScoped
 public class JacisProvider {
 
+  private static final java.util.logging.Logger log = java.util.logging.Logger.getLogger(JacisProvider.class.getName());
+
+  @ConfigProperty(name = "app.startRestServer", defaultValue = "false")
+  boolean startRestServer;
+
   @Produces
   @ApplicationScoped
   public JacisContainer produceJacisContainer(TransactionManager txManager) {
+    log.info("create JacisContainer...");
     return new JacisContainer(new JacisJtaAdapter(txManager));
   }
 
@@ -25,7 +34,12 @@ public class JacisProvider {
   public MicrostreamStorage produceMicrostreamStorage() {
     EmbeddedStorageManager storageManager = createMicroStreamStorageManager();
     storageManager.start();
+    if (startRestServer) {
+      StorageRestService restService = StorageRestServiceResolver.resolve(storageManager);
+      restService.start();
+    }
     MicrostreamStorage storage = new MicrostreamStorage(storageManager);
+    log.info("created Microstream Storage...");
     return storage;
   }
 
